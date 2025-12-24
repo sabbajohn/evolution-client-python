14eca0359382cdf009ca0041cb3fe5aa1f359e8c
John. Sabbá <victorsabba@gmail.com>
2025-12-24 15:40:30 -0300

    feat: Add webhook and event management methods to InstanceService

--- evolutionapi/services/instance.py
+++ evolutionapi/services/instance.py
@@ -7,3 +7,12 @@ class InstanceService:
 
     def create_instance(self, config):
         return self.client.post('instance/create', data=config.__dict__)
+
+    def set_webhook(self, instance_name: str, config, instance_token):
+        data = {'webhook': config.__dict__}
+        return self.client.post(f"webhook/set/{instance_name}", data=data)
+
+    def set_events(self, instance_name: str, config, instance_token: str):
+        return self.client.post(
+            f'instance/{instance_name}/{instance_token}/setEvents', data=config.__dict__
+        )
